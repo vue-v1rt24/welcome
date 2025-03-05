2d6677e54634d1fdf2be6e7996b8be5c7dc792b2
vue-v1rt24 <rublevrus@mail.ru>
2025-03-06 01:41:16 +0300

    Продолжение

--- components/header/search/Filters/Novostroiki.vue
+++ components/header/search/Filters/Novostroiki.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log(222);
+</script>
 
 <template>
   <div>Новостройки</div>
--- components/header/search/Filters/index.vue
+++ components/header/search/Filters/index.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
 import { LazyHeaderSearchFiltersKvartirs } from '#components';
 
-const currentComponent = shallowRef(LazyHeaderSearchFiltersKvartirs);
+// const currentComponent = shallowRef(LazyHeaderSearchFiltersKvartirs);
+const comp = ref<'Первый' | 'Второй' | 'Третий' | 'Четвёртый' | 'Пятый' | 'Шестой'>('Первый');
+
+const currentComp = computed(() =>
+  comp.value === 'Первый'
+    ? resolveComponent('LazyHeaderSearchFiltersKvartirs')
+    : comp.value === 'Второй'
+    ? resolveComponent('LazyHeaderSearchFiltersNovostroiki')
+    : comp.value === 'Третий'
+    ? resolveComponent('LazyHeaderSearchFiltersDoma')
+    : comp.value === 'Четвёртый'
+    ? resolveComponent('LazyHeaderSearchFiltersUchastok')
+    : comp.value === 'Пятый'
+    ? resolveComponent('LazyHeaderSearchFiltersCommercheskaya')
+    : comp.value === 'Шестой'
+    ? resolveComponent('LazyHeaderSearchFiltersGaragi')
+    : 'div',
+);
 </script>
 
 <template>
   <div>
     <div class="filters_btn">
       <label>
-        <input type="radio" v-model="currentComponent" value="LazyHeaderSearchFiltersKvartirs" />
+        <input type="radio" v-model="comp" value="Первый" />
+        <input type="radio" v-model="comp" value="Второй" />
+        <input type="radio" v-model="comp" value="Третий" />
+        <input type="radio" v-model="comp" value="Четвёртый" />
+        <input type="radio" v-model="comp" value="Пятый" />
+        <input type="radio" v-model="comp" value="Шестой" />
       </label>
     </div>
 
     <!--  -->
-    <Transition name="fade" mode="out-in">
+    <Transition name="slide-fade" mode="out-in">
       <KeepAlive>
-        <component :is="currentComponent" />
+        <component :is="currentComp" />
       </KeepAlive>
     </Transition>
   </div>
@@ -31,13 +53,29 @@ const currentComponent = shallowRef(LazyHeaderSearchFiltersKvartirs);
 </template>
 
 <style lang="css" scoped>
-.fade-enter-active,
+/* .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
+  opacity: 0;
+} */
+
+/*  */
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
   opacity: 0;
 }
 </style>
