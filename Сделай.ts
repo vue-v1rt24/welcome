1e2d1a1a2437fb3e736de7a1f0fe5361fe690cd7
vue-v1rt24 <rublevrus@mail.ru>
2025-03-12 03:04:19 +0300

    Файл для повторения. К проекту не относится

---
+++ Сделай.ts
@@ -0,0 +1,14 @@
+const data = [
+  { title: '', desc: '' },
+  { title: '', desc: '' },
+  { title: '', desc: '' },
+  { title: '', desc: '' },
+  { title: '', desc: '' },
+];
+
+const selected = ref('');
+
+// const res = data.filter(({ title, desc }) => title === selected.value || desc === selected.value);
+
+//
+// const res2 = data.filter(({ title }) => title.length).map(({ title }) => title);
