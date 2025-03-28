0501962bda0698d38a702fd25a8ae54b03bd76be
vue-v1rt24 <rublevrus@mail.ru>
2025-03-28 10:13:20 +0300

    Пока финал

--- pages/realty/[id].vue
+++ pages/realty/[id].vue
@@ -4,16 +4,11 @@ const route = useRoute();
 //
 const { data: realty } = await useFetch(`/api/pages/realty/${route.params.id}`);
 
-// console.log(realty.value);
+console.log(realty.value);
 </script>
 
 <template>
   <div>Страница недвижимости / {{ route.params.id }}</div>
-
-  <!-- Другие проекты -->
-  <div class="container">
-    <SlidersOtherProjects />
-  </div>
 </template>
 
 <style lang="css" scoped></style>
